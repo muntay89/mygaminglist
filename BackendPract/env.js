c9a46ada982203666ef262c52ff911538bbed3e6
muntay89 <117422395+muntay89@users.noreply.github.com>
2026-07-28 08:52:44 -0700

    fixed dotenv

--- BackendPract/env.js
+++ BackendPract/env.js
@@ -1,5 +1,5 @@
 // env.js
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
 import fs from "fs";
@@ -9,6 +9,10 @@ const __dirname = path.dirname(__filename);
 
 const envPath = path.join(__dirname, ".env");
 
-
-const result = dotenv.config({ path: envPath });
+if (process.env.NODE_ENV !== "production") {
+  const dotenv = await import("dotenv")
+  dotenv.config()
+  const result = dotenv.config({ path: envPath });
+}
+// const result = dotenv.config({ path: envPath });
 
