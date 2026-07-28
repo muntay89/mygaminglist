3a055464fb4ceb73cc8a82a666d2ac1e7d7ca0a9
muntay89 <117422395+muntay89@users.noreply.github.com>
2026-07-28 09:08:10 -0700

    Reroute base link to Homepage (fixed)

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState, useRef } from 'react'
 import {
   BrowserRouter as Router,
-  Routes, Route, } from 'react-router-dom'
+  Routes, Route, Navigate} from 'react-router-dom'
 import './App.css'
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
