4aabb87b28077498030c35d096a6138a96ca4567
muntay89 <117422395+muntay89@users.noreply.github.com>
2026-08-16 20:52:39 -0700

    Finalize MyGamingList portfolio release

--- BackendPract/env.js
+++ BackendPract/env.js
@@ -1,18 +1,15 @@
 // env.js
-// import dotenv from "dotenv";
-import path from "path";
-import { fileURLToPath } from "url";
-import fs from "fs";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import path from "path"
+import { fileURLToPath } from "url"
 
-// const envPath = path.join(__dirname, ".env");
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 if (process.env.NODE_ENV !== "production") {
   const dotenv = await import("dotenv")
-  dotenv.config()
-  const result = dotenv.config({ path: envPath });
-}
-const result = dotenv.config({ path: envPath });
 
+  dotenv.config({
+    path: path.join(__dirname, ".env")
+  })
+}
